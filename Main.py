51882c5009e66f02f9b460982261daa37f1948df
ChetanLingareddy <lingareddychetan@gmail.com>
2025-01-26 13:49:19 -0600

    initial code

--- Main.py
+++ Main.py
@@ -1,3 +1,9 @@
+def get_todos():
+    with open ( "Todo.txt", 'r' ) as file_local :
+        todos_local = file_local.readlines ()
+    return todos_local
+
+
 while True:
     user_action= input("Type add, show, edit, complete or exit : ")
     user_action = user_action.strip()
@@ -14,8 +20,7 @@ while True:
         # Below code is the modified version of the above traditional code which is error free
         todo = "\n" + user_action[4:]
 
-        with open("Todo.txt",'r')  as file:
-            todos = file.readlines()
+        todos = get_todos()
 
         todos.append(todo)
         #this code lines are for writing text in to the text files in traditional way
@@ -34,8 +39,7 @@ while True:
         todos = file.readlines()
         file.close() 
         """
-        with open("Todo.txt",'r')  as file:
-            todos = file.readlines()
+        todos = get_todos ()
 
         for index,x in enumerate(todos):
             x= x.capitalize()
@@ -47,8 +51,7 @@ while True:
             number  = int(user_action[5:])
             new_number = number -1
 
-            with open("Todo.txt",'r') as file:
-                todos = file.readlines()
+            todos = get_todos()
 
             new_todo= input("enter your new To-do: ")
             todos[new_number]=new_todo + "\n"
@@ -63,8 +66,7 @@ while True:
         try:
             number = int(user_action[9:])
 
-            with open("Todo.txt",'r') as file:
-                todos = file.readlines()
+            todos = get_todos()
 
             index = number -1
             Item_to_remove = todos[index].strip()
