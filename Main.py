86b86246991e2f878647d46657fa6f6d260be192
ChetanLingareddy <lingareddychetan@gmail.com>
2025-01-27 20:07:00 -0600

    initial code

--- Main.py
+++ Main.py
@@ -1,15 +1,8 @@
-def get_todos(filepath = "Todo.txt"):
-    """Reads a text file and returns a list of to- do items."""
-    with open ( filepath, 'r' ) as file_local :
-        todos_local = file_local.readlines ()
-    return todos_local
+from functions import *
+import time
 
-def write_todos(todos_arg,filepath = "Todo.txt" ):
-    """ Writes the to-do items list to the text file."""
-    with open(filepath, 'w') as file:
-        file.writelines(todos_arg)
-
-
+now = time.strftime("%B %d %Y (%H: %M: %S)")
+print(now)
 while True:
     user_action= input("Type add, show, edit, complete or exit : ")
     user_action = user_action.strip()
