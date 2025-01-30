400324bb23fe14336278c8db80d831cd9c6113f7
ChetanLingareddy <lingareddychetan@gmail.com>
2025-01-30 09:45:08 -0600

    Implement Edit  button

--- web.py
+++ web.py
@@ -9,8 +9,13 @@ def add_todo():
     functions.write_todos(todos)
 
 st.title("My Todo List ☑️")
-for todo in todos:
-    st.checkbox(todo)
+for index, todo in enumerate(todos):
+    checkbox = st.checkbox(todo, key = todo)
+    if checkbox:
+        todos.pop(index)
+        functions.write_todos(todos)
+        del st.session_state[todo]
+        st.rerun()
 st.text_input(label= "", placeholder="Enter a new Todo.....",
               on_change=add_todo, key="new_todo")
 
