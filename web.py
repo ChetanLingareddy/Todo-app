e9a8e708568ae633a30cf46642411db83bf9090f
ChetanLingareddy <lingareddychetan@gmail.com>
2025-01-30 09:29:25 -0600

    Implement Edit  button

---
+++ web.py
@@ -0,0 +1,19 @@
+import streamlit as st
+import functions
+
+todos = functions.get_todos()
+
+def add_todo():
+    todo = st.session_state["new_todo"] + "\n"
+    todos.append(todo)
+    functions.write_todos(todos)
+
+st.title("My Todo List ☑️")
+for todo in todos:
+    st.checkbox(todo)
+st.text_input(label= "", placeholder="Enter a new Todo.....",
+              on_change=add_todo, key="new_todo")
+
+
+
+
